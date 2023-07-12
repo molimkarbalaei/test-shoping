906625de60312c89c1a794d44b3a73e278594f7f
Molim Karbalaei <molim@saco.info>
2023-07-12 19:03:01 +0200

    Add: adding navigation with event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // make sure that we store this feautre
+  loadedFeature = 'recipe';
+
+  // in here where we should navigate to the information and choose any name you want:
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
