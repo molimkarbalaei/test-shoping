e991ed448d0f28c1b6cecad02ba08808452c4a5f
Molim Karbalaei <molim@saco.info>
2023-07-14 09:28:29 +0200

    update: close dropdown by clicking anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,15 +1,24 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
 // Adding some functionality to it to allows us to add certain css class to the element it sits on when clicked! and remove again when we clicked again!
 export class DropdownDirective {
-    // to change it dinamically and bind it to the 'class' of the ELEMENT => we use @HoistBinding
+  constructor(private elRef: ElementRef) {}
+
+  // to change it dinamically and bind it to the 'class' of the ELEMENT => we use @HoistBinding
   @HostBinding('class.open') isOpen = false;
 
   //Toggling so we need to listen to it.
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
 }
