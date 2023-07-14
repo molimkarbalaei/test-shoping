d15a0499c157c400f6d5aebe97888e371d2bf9a9
Molim Karbalaei <molim@saco.info>
2023-07-14 15:06:23 +0200

    add: ingredients to recipe

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,14 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 //Blue print:
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[]
 
   // Using constructor for instantiate this with the new keyword and pass it to the constructor
-  constructor(name: string, desc: string, imagePath: string) {
-    (this.name = name), (this.description = desc), (this.imagePath = imagePath);
+  constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
+    (this.name = name), (this.description = desc), (this.imagePath = imagePath), (this.ingredients = ingredients);
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable()
 export class Recipeservice {
@@ -9,12 +10,20 @@ export class Recipeservice {
     new Recipe(
       'A Test Recipe',
       'This is a simply a test',
-      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
+      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Onion', 1)
+      ]
     ),
     new Recipe(
-      'Recipe here',
-      'This is a simply a test',
-      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
+      'Italian food ;)',
+      'Is it Lasagna?',
+      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg',
+      [new Ingredient('Meat', 2),
+      new Ingredient('Lasagna', 1),
+      new Ingredient('Tomato', 3)    
+    ]
     ),
   ];
   getRecipes() {
