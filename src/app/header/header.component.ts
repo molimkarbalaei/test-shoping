0aa47923e1755dc17146d6322317d32648ea8246
Molim Karbalaei <molim@saco.info>
2023-07-18 12:35:50 +0200

    feat: add dynamic link class

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,9 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.componet.html',
 })
 export class HeaderComponent{
-    collapsed = true;
-   @Output() featureSelected = new EventEmitter<string>()
 
-
-    onSelect(feature: string){
-    this.featureSelected.emit(feature)
-    }
 }
