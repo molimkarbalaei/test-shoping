a0b691250ccebe8d961c2518a1bbb42defee747d
Molim Karbalaei <molim@saco.info>
2023-07-18 14:22:38 +0200

    add: passing dynamic parameter to link by index

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent implements OnInit {
   // get the recipe from the outside initially:
   @Input() recipe: Recipe;
-
+  //To pass the id, we have to pass this info to the component and send it to the recipes-list and also pass this index in recipe detail tempelate.
+  @Input() index: number;
   ngOnInit() {}
 }
 
